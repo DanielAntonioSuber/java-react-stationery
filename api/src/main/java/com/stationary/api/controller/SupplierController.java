4b1156902cfb98dd8a2a8db9c467a8827f17e250
DanielAntonioSuber <antonio.eugenio.daniel1@gmail.com>
2022-09-05 14:02:20 -0500

    Supplier controller

---
+++ api/src/main/java/com/stationary/api/controller/SupplierController.java
@@ -0,0 +1,53 @@
+package com.stationary.api.controller;
+
+import com.stationary.api.dto.SupplierDto;
+import com.stationary.api.dto.SuppliersResponse;
+import com.stationary.api.service.SupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(name = "/api/supplier")
+public class SupplierController {
+
+    @GetMapping
+    public SuppliersResponse getSuppliers(
+            @RequestParam(name = "pageNumber", value = "0") Integer pageNumber,
+            @RequestParam(name = "sizePage", value = "10") Integer sizePage,
+            @RequestParam(name = "sortBy", value = "id") String sortBy,
+            @RequestParam(name = "sortDir", value = "ASC") String sortDir
+    ) {
+        return supplierService.getSuppliers(pageNumber, sizePage, sortBy, sortDir);
+    }
+
+    @GetMapping("/{supplierId}")
+    public SupplierDto getSupplierById(@PathVariable(name = "supplierId") Integer supplierId) {
+        return supplierService.getSupplierById(supplierId);
+    }
+
+    @PostMapping
+    public ResponseEntity<SupplierDto> createSupplier(@Valid @RequestBody SupplierDto supplierDto) {
+        SupplierDto newSupplier = supplierService.addSupplier(supplierDto);
+        return new ResponseEntity<>(newSupplier, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{supplierId}")
+    public ResponseEntity<String> deleteSupplierById(@PathVariable(name = "supplierId") Integer supplierId) {
+        supplierService.deleteSupplierById(supplierId);
+        return new ResponseEntity<>("Supplier was deleted", HttpStatus.OK);
+    }
+
+    @PutMapping("/{supplierId}")
+    public ResponseEntity<SupplierDto> updateSupplier(@PathVariable(name = "supplierId") Integer supplierId, @Valid @RequestBody SupplierDto supplierDto) {
+        SupplierDto updatedSupplier = supplierService.updateSupplier(supplierId, supplierDto);
+        return new ResponseEntity<>(updatedSupplier, HttpStatus.OK);
+    }
+
+    @Autowired
+    private SupplierService supplierService;
+
+}
--- api/src/main/java/com/stationary/api/dto/SupplierDto.java
+++ api/src/main/java/com/stationary/api/dto/SupplierDto.java
@@ -3,13 +3,20 @@ package com.stationary.api.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter @Setter
 public class SupplierDto {
 
     private Integer id;
 
+    @NotBlank
     private String supplierName;
 
+    @NotBlank
+    //@Pattern(regexp = "[a-zA-Z]{3,4}(\\d{6})((Dd){2,3})?")
+    @Size(min = 12, max = 13)
     private String rfc;
 
 }
